d95f9dd929e3434e0a15bb3642d114d1d7f0ffb5
duddnd11 <yjk8003@gmail.com>
2025-04-04 00:05:16 +0900

    Payment

---
+++ src/main/java/kr/hhplus/be/server/payment/PaymentController.java
@@ -0,0 +1,25 @@
+package kr.hhplus.be.server.payment;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="Payment API", description = "결제 관련")
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+	/**
+	 * 결제
+	 * @param paymentRequest
+	 * @return
+	 */
+	@PostMapping
+	public ResponseEntity<PaymentResponse> payment(@RequestBody PaymentRequest paymentRequest){
+		return ResponseEntity.ok(new PaymentResponse(1L, PaymentStatus.COMPLETED));
+	}
+}
