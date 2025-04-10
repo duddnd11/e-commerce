6a5cbd85d599529dca866c6b5ca9c0a79b681083
duddnd11 <yjk8003@gmail.com>
2025-04-10 23:43:48 +0900

    product test

---
+++ src/test/java/kr/hhplus/be/server/domain/ProductTest.java
@@ -0,0 +1,46 @@
+package kr.hhplus.be.server.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import kr.hhplus.be.server.domain.product.Product;
+
+public class ProductTest {
+	
+	@Test
+	@DisplayName("재고 차감")
+	void deductStock() {
+		// given
+		Product product = new Product("모니터", 100000, 100);
+		
+		// when
+		product.deductStock(100);
+		
+		//then
+		assertThat(product.getStock()).isEqualTo(0);
+	}
+	
+	@Test
+	@DisplayName("재고 부족")
+	void deductStockError() {
+		// given
+		Product product = new Product("모니터", 100000, 10);
+		
+		// when & then
+		assertThrows(IllegalArgumentException.class, () -> product.deductStock(11));
+	}
+	
+	@Test
+	@DisplayName("재고 차감 수량 0 이하 에러")
+	void deductStockError2() {
+		// given
+		Product product = new Product("모니터", 100000, 10);
+		
+		// when & then
+		assertThrows(IllegalArgumentException.class, () -> product.deductStock(0));
+	}
+}
