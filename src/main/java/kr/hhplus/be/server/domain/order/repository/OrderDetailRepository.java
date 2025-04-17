bf339e9805733628c124805ff67b62c7eaaa8aff
duddnd11 <yjk8003@gmail.com>
2025-04-17 20:31:47 +0900

    order 리팩토링

--- src/main/java/kr/hhplus/be/server/domain/order/repository/OrderDetailRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/order/repository/OrderDetailRepository.java
@@ -1,7 +1,10 @@
 package kr.hhplus.be.server.domain.order.repository;
 
+import java.util.List;
+
 import kr.hhplus.be.server.domain.order.entity.OrderDetail;
 
 public interface OrderDetailRepository {
 	OrderDetail save(OrderDetail orderDetail);
+	List<OrderDetail> findByOrderId(Long orderId);
 }
