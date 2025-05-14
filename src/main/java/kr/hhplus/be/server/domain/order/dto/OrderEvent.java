06587f00afc015d5a77a146b35e7fd252a03dd0e
duddnd11 <yjk8003@gmail.com>
2025-05-14 23:33:06 +0900

    redis 저장을 위한 이벤트 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/order/dto/OrderEvent.java
@@ -0,0 +1,12 @@
+package kr.hhplus.be.server.domain.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEvent {
+	long orderId;
+}
